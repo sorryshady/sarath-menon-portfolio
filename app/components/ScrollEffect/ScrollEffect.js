b40d68ff16eb8216127bb4b84f21a433be32bed4
Akhil Nishad <akhilnishadofficial@gmail.com>
2023-12-24 16:43:40 +0530

    font size changes

--- app/components/ScrollEffect/ScrollEffect.js
+++ app/components/ScrollEffect/ScrollEffect.js
@@ -30,7 +30,7 @@ const Column = ({ images, y = 0 }) => {
       {images.map((src, index) => {
         return (
           <div key={index} className={styles.imageContainer}>
-            <Image src={src} fill alt='image' layout='fill' priority />
+            <Image src={src} fill alt='image' priority />
           </div>
         )
       })}
