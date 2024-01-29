f6429b05d337ca1303f09c0abbcad2d9455e0bd8
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-01-29 20:39:16 +0530

    cleanup header

--- app/components/Nav/Header.js
+++ app/components/Nav/Header.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import Link from 'next/link'
 import { useWindowSize } from 'rooks'
@@ -9,8 +9,27 @@ import Button from './Button/Button'
 import NavLinks from './NavLinks/NavLinks'
 const Header = () => {
   const { innerWidth: inner } = useWindowSize()
+  const [isClient, setIsClient] = useState(false)
   const [isActive, setIsActive] = useState(false)
-  const variants = {
+  const [initialStyles, setInitialStyles] = useState({
+    width: inner > 425 ? 50 : 40,
+    height: inner > 425 ? 50 : 40,
+    top: '0px',
+    right: '0px',
+  })
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+  useEffect(() => {
+    setInitialStyles({
+      width: inner > 425 ? 50 : 40,
+      height: inner > 425 ? 50 : 40,
+      top: '0px',
+      right: '0px',
+    })
+  }, [inner])
+
+  const menuVariants = {
     open: {
       width: inner > 768 ? 480 : 250,
       height: inner > 768 ? 370 : 270,
@@ -18,42 +37,41 @@ const Header = () => {
       right: inner > 768 ? '-10px' : '-5px',
       transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] },
     },
-    closed: {
-      width: inner > 425 ? 50 : 40,
-      height: inner > 425 ? 50 : 40,
-      top: '0px',
-      right: '0px',
-      transition: { duration: 0.75, delay: 0.35, ease: [0.76, 0, 0.24, 1] },
-    },
+    closed: initialStyles,
   }
   return (
     <>
-      <header className={styles.header}>
-        <Link href={'/'}>
-          <div className={styles.logo}>
-            <Logo />
+      {isClient && (
+        <>
+          <header className={styles.header}>
+            <Link href={'/'}>
+              <div className={styles.logo}>
+                <Logo />
+              </div>
+            </Link>
+            <Link href={'/'}>
+              <div className={styles.name}>
+                <div>SARATH MENON</div>
+                <div>ARTIST | FILMMAKER</div>
+              </div>
+            </Link>
+          </header>
+          <div className={styles.menuContainer}>
+            <motion.div
+              style={initialStyles}
+              variants={menuVariants}
+              animate={isActive ? 'open' : 'closed'}
+              className={styles.menu}
+              initial='closed'
+            >
+              <AnimatePresence>
+                {isActive && <NavLinks setIsActive={setIsActive} />}
+              </AnimatePresence>
+            </motion.div>
+            <Button setIsActive={setIsActive} isActive={isActive} />
           </div>
-        </Link>
-        <Link href={'/'}>
-          <div className={styles.name}>
-            <div>SARATH MENON</div>
-            <div>ARTIST | FILMMAKER</div>
-          </div>
-        </Link>
-      </header>
-      <div className={styles.menuContainer}>
-        <motion.div
-          variants={variants}
-          animate={isActive ? 'open' : 'closed'}
-          className={styles.menu}
-          initial='closed'
-        >
-          <AnimatePresence>
-            {isActive && <NavLinks setIsActive={setIsActive} />}
-          </AnimatePresence>
-        </motion.div>
-        <Button setIsActive={setIsActive} isActive={isActive} />
-      </div>
+        </>
+      )}
     </>
   )
 }
