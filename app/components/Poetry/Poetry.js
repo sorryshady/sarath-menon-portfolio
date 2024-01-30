d95270b065201f04dde43df95ad4fc82abc4ff8b
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-01-30 19:14:46 +0530

    optimised poetry section image

--- app/components/Poetry/Poetry.js
+++ app/components/Poetry/Poetry.js
@@ -3,20 +3,14 @@ import styles from "./poetry.module.css";
 import Link from "next/link";
 import Image from 'next/image'
 import Heading from '@/app/utils/Heading/Heading'
+import poem from '/public/Images/poem/img_2.jpg'
 const Poetry = () => {
   return (
     <>
       <Heading text='Poetry' id={'poems'} />
       <div className={`container ${styles.poemWrapper}`}>
         <div className={styles.image}>
-          <Image
-            src='/Images/poem/img_2.jpg'
-            alt='image'
-            width={500}
-            height={500}
-            quality={100}
-            priority
-          />
+          <Image src={poem} alt='image' placeholder='blur' />
         </div>
         <div className={styles.poem}>
           <p className={styles.content}>
